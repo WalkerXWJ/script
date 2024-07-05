6fca14ef00bf3d9be43bb4a832c04736a80a21b6
feidou <42584283+WalkerXWJ@users.noreply.github.com>
2024-07-05 12:53:49 +0800

    Create termux-init.sh

---
+++ termux-init.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+echo "********************将termux的源更换为国内源********************"
+# 更换国内源
+sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
+sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@' $PREFIX/etc/apt/sources.list.d/game.list
+sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/science-packages-24 science stable@' $PREFIX/etc/apt/sources.list.d/science.list
+pkg update && pkg upragde
+# 安装常用工具软件
+echo '********************开始安装常用工具********************';
+apt install vim git 
