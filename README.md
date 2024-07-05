2315bccb315299797420f6233cca833688d21619
feidou <42584283+WalkerXWJ@users.noreply.github.com>
2024-07-05 23:01:10 +0800

    update kali-init and readme
    update kali-init and readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # script
  Some useful script files。
+
+## kali-init
+
+`kali-init.sh` kali linux 初次使用时的初始化脚本
+
+## termux-init
+
+`termux-init.sh`termux 初次使用时的初始化脚本
+## win-init
+
+`win-init.bat` windows 初次使用时的初始化脚本
--- kali-init.sh
+++ kali-init.sh
@@ -4,7 +4,14 @@
 kail liunx语言环境必须是英文才能运行此脚本
 According to my personal habits, write this bash script file to install the tool when you first use Kail Linux.
 '
-
+apt install
+echo "> 开始安装msf工具可视化管理软件 -- armitage --"
+sleep 3
+apt install armitage
+echo -e "\e[1;34m初次使用armitage,先执行 msfdb init\e[0m"
+sleep 3
+msfdb init
+echo "> armitage intall end....................."
 # github项目:
 
 # github项目:
