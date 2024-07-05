7bdf534eb7c1411fa4e39a5195115fc2bfb995f5
feidou <42584283+WalkerXWJ@users.noreply.github.com>
2024-07-05 14:24:22 +0800

    add kali-init.sh

---
+++ kali-init.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+: '
+根据个人使用习惯，编写此bash脚本文件，用于首次使用kail linux时安装工具
+kail liunx语言环境必须是英文才能运行此脚本
+According to my personal habits, write this bash script file to install the tool when you first use Kail Linux.
+'
+
+# github项目:
+
+# github项目:
+
+# github项目:
+
+# github项目:
+
+# github项目:
+
+# github项目:
+
+# github项目:
+
+# github项目:
+
+# github项目:
+
+# github项目:
+
+# github项目:
--- termux-init.sh
+++ termux-init.sh
@@ -4,7 +4,15 @@ echo "********************将termux的源更换为国内源********************"
 sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
 sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@' $PREFIX/etc/apt/sources.list.d/game.list
 sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/science-packages-24 science stable@' $PREFIX/etc/apt/sources.list.d/science.list
-pkg update && pkg upragde
+pkg update -y && pkg upragde -y
 # 安装常用工具软件
 echo '********************开始安装常用工具********************';
-apt install vim git 
+apt install vim git nmap wget curl htop -y
+
+# 终端配色 脚本项目地址：https://github.com/Cabbagec/termux-ohmyzsh/
+# 配色命令方案1 （github地址）： sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
+# 配色命令方案2 （国光私服地址）：sh -c "$(curl -fsSL https://html.sqlsec.com/termux-install.sh)"
+#设置色彩样式：输入 chcolor 命令更换色彩样式，或者执行 ~/.termux/colors.sh 命令
+#设置字体：运行 chfont 命令更换字体，或者执行 ~/.termux/fonts.sh 命令
+# 问候语修改 vim $PREFIX/etc/motd
+echo '********************安装一些github项目********************';
